d342cb4ab64c415030a0daf86444c40a5985af8e
Mark Palotas <ppmark@gmail.com>
2018-10-29 16:14:51 +0100

    Added exercises up to and including Nr 22

---
+++ week-02/day-01/src/PrintBigger.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PrintBigger {
+    public static void main(String[] args) {
+        // Write a program that asks for two numbers and prints the bigger one
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter two integers, pressing Enter after each one: ");
+
+        int firstNum = scanner.nextInt();
+
+        int secondNum = scanner.nextInt();
+
+        int difference = (firstNum - secondNum);
+
+        if (difference > 0) {
+
+            System.out.println("This is the bigger number: " + firstNum);
+
+            }
+
+        else if (difference < 0) {
+
+            System.out.println("This is the bigger number: " + secondNum);
+
+        }
+
+        else {
+
+            System.out.println("These two numbers are equal.");
+
+        }
+    }
+}
