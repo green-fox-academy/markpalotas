e77d512462e9be1b0f4efcb32fd215860eeb12b2
Mark Palotas <ppmark@gmail.com>
2018-10-29 12:55:45 +0100

    Added Exercises 1-4 for the day

---
+++ week-02/day-01/src/HelloOthers.java
@@ -0,0 +1,13 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+        System.out.println("Hello Babo!");
+        System.out.println("Hello Dorcsa!");
+        System.out.println("Hello Jozef!");
+    }
+}
