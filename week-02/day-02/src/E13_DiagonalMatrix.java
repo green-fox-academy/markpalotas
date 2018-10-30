8f0f7ea7c9533f58f878214bd3eab9d54cc56e5c
Mark Palotas <ppmark@gmail.com>
2018-10-30 14:42:12 +0100

    Added exercises for Week02 Day02 up to and incl Nr 13

---
+++ week-02/day-02/src/E13_DiagonalMatrix.java
@@ -0,0 +1,14 @@
+public class E13_DiagonalMatrix {
+    public static void main(String[] args) {
+        // - Create (dynamically) a two dimensional array
+        //   with the following matrix. Use a loop!
+        //
+        //   1 0 0 0
+        //   0 1 0 0
+        //   0 0 1 0
+        //   0 0 0 1
+        //
+        // - Print this two dimensional array to the output
+        
+    }
+}
