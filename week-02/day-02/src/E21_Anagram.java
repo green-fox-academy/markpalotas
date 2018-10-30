de927f7549bc8f3195aee9e577469f721eecfb12
Mark Palotas <ppmark@gmail.com>
2018-10-30 17:40:05 +0100

    Added Exercises up to 21 with 20 still missing / Week02 Day02

---
+++ week-02/day-02/src/E21_Anagram.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class E21_Anagram {
+    public static void main(String[] args) {
+        // Create a function to check if the two inputs it receives are anagrams of each other
+        System.out.println(anagram("god", "dog"));
+    }
+
+    public static boolean anagram(String inputA, String inputB) {
+        boolean isAnagram = false;
+        if (inputA.length() != inputB.length()) {
+            isAnagram = false;
+        } else {
+            String[] inA = new String[inputA.length()];
+            String[] inB = new String[inputB.length()];
+            for (int i = 0; i < inputA.length(); i++) {
+                inA[i] = inputA.substring(i, i + 1);
+            }
+            for (int j = 0; j < inputB.length(); j++) {
+                inB[j] = inputB.substring(j, j + 1);
+            }
+            Arrays.sort(inA);
+            Arrays.sort(inB);
+            String sortedA = "";
+            String sortedB = "";
+            for (int k = 0; k < inA.length; k++) {
+                sortedA = sortedA + inA[k];
+            }
+            for (int l = 0; l < inB.length; l++) {
+                sortedB = sortedB + inB[l];
+            }
+            if (sortedA.equals(sortedB)) {
+                isAnagram = true;
+            }
+            System.out.println(sortedA + " " + sortedB);
+            System.out.println(Arrays.toString(inA));
+            System.out.println(Arrays.toString(inB));
+        }
+        return isAnagram;
+    }
+}
