1e1417d71e285ecb1e4ed485f7b241aac8e75600
Mark Palotas <ppmark@gmail.com>
2018-10-30 13:55:10 +0100

    Added Week02 Day02 exercises up to and including Nr 7

---
+++ week-02/day-02/src/E05_Factorio.java
@@ -0,0 +1,18 @@
+public class E05_Factorio {
+    public static void main(String[] args) {
+        //  Create the usual class wrapper
+        //  and main method on your own.
+
+        // - Create a function called `factorio`
+        //   that returns it's input's factorial
+        System.out.println(factorio(5));
+    }
+
+    public static int factorio(int a) {
+        int factorio = 1;
+        for (int i = a; i > 0; i--) {
+            factorio = factorio * i;
+        }
+        return factorio;
+    }
+}
