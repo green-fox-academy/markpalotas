ede8732e03634809fc96cb16de163913ac056447
Mark Palotas <ppmark@gmail.com>
2018-10-30 08:37:49 +0100

    Completed Week02 Day01 E28

--- week-02/day-01/src/E28DrawTriangle.java
+++ week-02/day-01/src/E28DrawTriangle.java
@@ -18,11 +18,24 @@ public class E28DrawTriangle {
 
         int yourNum = scanner.nextInt();
 
-        for (int i = 1; i < (yourNum+1); i++) {
-            for (int j = i; j < (yourNum + 1); j++) {
+        for (int i = 1; i < (yourNum + 1); i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
-            System.out.println("");
+            System.out.println();
         }
+
+        // A simpler solution (hopefully a working one)
+
+        System.out.println("Please enter another number:");
+
+        yourNum = scanner.nextInt();
+
+        String star = "*";
+
+        for (int i = 1; i < (yourNum + 1); i++) {
+            System.out.print(star + "\n");
+            star = star + "*";
+            }
     }
 }
