d690bbdae7e7e9cd7996f1d15b850d54dc132e31
Mark Palotas <ppmark@gmail.com>
2018-10-31 15:42:17 +0100

    Added further W02D03 practice exercises

---
+++ week-02/day-03/src/W02D01E31_DrawSquare.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class W02D01E31_DrawSquare {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+        // square like this:
+        //
+        //
+        // %%%%%%
+        // %    %
+        // %    %
+        // %    %
+        // %    %
+        // %%%%%%
+        //
+        // The square should have as many lines as the number was
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What should be the height of your square?");
+
+        int sqHeight = scanner.nextInt();
+
+        for (int i = 0; i < sqHeight; i++) {
+            System.out.print("%");
+        }
+        System.out.println();
+        for (int i = 0; i < sqHeight - 2; i++) {
+            System.out.print("%");
+            for (int j = 0; j < sqHeight - 2; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("%");
+            System.out.println();
+        }
+        for (int i = 0; i < sqHeight; i++) {
+            System.out.print("%");
+        }
+    }
+}
