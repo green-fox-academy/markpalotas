e2d51e4e9f3934c850cf4d3c8a63ea5f634fe784
Mark Palotas <ppmark@gmail.com>
2018-11-08 10:20:21 +0100

    Added W02D01E33 GuessTheNumber

--- week-02/day-03/src/W02D01E33_GuessTheNumber.java
+++ week-02/day-03/src/W02D01E33_GuessTheNumber.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class W02D01E33_GuessTheNumber {
     public static void main(String[] args) {
         // Write a program that stores a number, and the user has to figure it out.
@@ -7,5 +9,19 @@ public class W02D01E33_GuessTheNumber {
         // The stored number is higher
         // The stried number is lower
         // You found the number: 8
+        int rand = (int) (Math.random() * 101);
+        Scanner scanner = new Scanner(System.in);
+        int guess = 100;
+
+        do {
+            System.out.println("Can you guess which number I'm thinking of? It's between 0 and 100.");
+            guess = scanner.nextInt();
+            if (guess > rand) {
+                System.out.println("Go for a smaller number!");
+            } else if (guess < rand) {
+                System.out.println("Go for a bigger one!");
+            }
+        } while (guess != rand);
+        System.out.println("You guessed it right! The number indeed was " + rand + ".");
     }
 }
---
+++ week-03/day-04/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        for (int i = 0; i < 15 ; i++) {
+            int n = (int) (Math.random() * 100);
+            System.out.println(n);
+        }
+    }
+}
