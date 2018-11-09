3a107a6c7e5c5ac7234fb70bb9cdb2a40c6b1f54
Mark Palotas <ppmark@gmail.com>
2018-11-09 12:06:05 +0100

    Added initial version of W03D05Project LinePlay_parametric

---
+++ week-03/day-05/src/W03D05_LinePlay_parametric.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class W03D05_LinePlay_parametric {
+    public static final Color PURPLE = new Color(102, 0, 153);
+    public static void mainDraw(Graphics graphics) {
+        int freq = 15;
+        graphics.setColor(PURPLE);
+        for (int i = 0; i < freq - 1; i++) {
+            drawLines(graphics, WIDTH, HEIGHT - (i + 1) * (HEIGHT / freq), WIDTH - (i + 1) * (WIDTH / freq), 0);
+        }
+//        graphics.setColor(Color.decode("#32CD32"));
+//        below: alternative method for providing color in hex
+        graphics.setColor(new Color(0x32CD32));
+        for (int i = 0; i < freq - 1; i++) {
+            drawLines(graphics, 0, (i + 1) * (HEIGHT / freq), (i + 1) * (WIDTH / freq), HEIGHT);
+        }
+    }
+
+    public static void drawLines(Graphics g, int x1, int y1, int x2, int y2) {
+        g.drawLine(x1, y1, x2, y2);
+    }
+
+    public static void drawLinePlay(Graphics g, int originX, int originY, int size, int freq) {
+        for (int i = 0; i < freq - 1; i++) {
+            drawLines(g, originX + (i + 1) * (size / freq), originY, originX, originY - (i + 1) * (size / freq));
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 800;
+    static int HEIGHT = 800;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
