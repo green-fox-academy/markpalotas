333688cfe9f5c359ca2060719c6bd17927987c5b
Mark Palotas <ppmark@gmail.com>
2018-11-14 16:42:42 +0100

    Added exercise W02D01 DrawChessTable

---
+++ week-04/day-03/src/W02D01_ParametricAverage/W02D01_DrawChessTable.java
@@ -0,0 +1,21 @@
+package W02D01_ParametricAverage;
+
+public class W02D01_DrawChessTable {
+    public static void main(String[] args) {
+        String a = "%";
+        String b = " ";
+        for (int i = 0; i < 8; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < 4; j++) {
+                    System.out.print(a + b);
+                }
+                System.out.print("\n");
+            } else {
+                for (int j = 0; j < 4; j++) {
+                    System.out.print(b + a);
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+}
