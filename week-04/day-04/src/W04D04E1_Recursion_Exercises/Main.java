b629b3955b2b71d6db7d163f66e475e603526ca6
Mark Palotas <ppmark@gmail.com>
2018-11-15 12:06:56 +0100

    Added recursion exercise 07 String1_v2 with a neater base case

--- week-04/day-04/src/W04D04E1_Recursion_Exercises/Main.java
+++ week-04/day-04/src/W04D04E1_Recursion_Exercises/Main.java
@@ -4,14 +4,10 @@ package W04D04E1_Recursion_Exercises;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(recursiveCounter(10));
-    }
-
-    public static String recursiveCounter(int n) {
-        if (n == 0) {
-            return "0\n";
-        } else {
-            return n + "\n" + recursiveCounter(n-1);
+        String string = "name";
+        for (int i = 0; i < string.length(); i++) {
+            System.out.println(string.substring(i, i + 1));
         }
     }
+
 }
---
+++ week-04/day-04/src/W04D04E1_Recursion_Exercises/String1_v2.java
@@ -0,0 +1,22 @@
+package W04D04E1_Recursion_Exercises;
+
+// Given a string, compute recursively (no loops) a new string where all the
+// lowercase 'x' chars have been changed to 'y' chars.
+
+public class String1_v2 {
+    public static void main(String[] args) {
+        System.out.println(string1("X"));
+    }
+
+    public static String string1(String string) {
+        if (string.equals("")) {
+            return string;
+            } else {
+                if (string.substring(0, 1).equals("x")) {
+                    return "y" + string1(string.substring(1));
+                } else {
+                    return string.substring(0, 1) + string1(string.substring(1));
+                }
+            }
+    }
+}
