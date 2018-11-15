5b96a2cdddd62fbbfed7e2dc79d3c55fc7519ef6
Mark Palotas <ppmark@gmail.com>
2018-11-15 16:56:24 +0100

    Started working on ReproduceThis1

--- week-04/day-04/src/W04D04E1_Recursion_Exercises/Refactorio.java
+++ week-04/day-04/src/W04D04E1_Recursion_Exercises/Refactorio.java
@@ -2,14 +2,17 @@ package W04D04E1_Recursion_Exercises;
 
 public class Refactorio {
     public static void main(String[] args) {
-        System.out.println(refactorio(5));
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            System.out.println(refactorio(i));
+        }
     }
 
     public static int refactorio(int n) {
         if (n == 0) {
             return 1;
         } else {
-            return n * refactorio(n - 1);
+             return n * refactorio(n - 1);
         }
     }
 }
---
+++ week-04/day-04/src/W04D04E1_Recursion_Exercises/ReproduceThis1.java
@@ -0,0 +1,113 @@
+package W04D04E1_Recursion_Exercises;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.rmi.MarshalException;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class ReproduceThis1 {
+    int counter = 0;
+
+    public static void mainDraw(Graphics graphics) {
+        int x0 = 0;
+        int y0 = 0;
+        int length = WIDTH / 3;
+        drawRectangles(graphics, x0, y0, length);
+        int x1 = x0 + length;
+        int y1 = y0;
+        int x2 = x0 + 2 * length;
+        int y2 = y0 + length;
+        int x3 = x0 + length;
+        int y3 = y0 + 2 * length;
+        int x4 = x0;
+        int y4 = y0 + length;
+        length = length / 3;
+        drawRectangles(graphics, x1, y1, length);
+        drawRectangles(graphics, x2, y2, length);
+        drawRectangles(graphics, x3, y3, length);
+        drawRectangles(graphics, x4, y4, length);
+        int x11 = x1 + length;
+        int y11 = y1;
+        int x12 = x1 + 2 * length;
+        int y12 = y1 + length;
+        int x13 = x1 + length;
+        int y13 = y1 + 2 * length;
+        int x14 = x1;
+        int y14 = y1 + length;
+        int x21 = x2 + length;
+        int y21 = y2;
+        int x22 = x2 + 2 * length;
+        int y22 = y2 + length;
+        int x23 = x2 + length;
+        int y23 = y2 + 2 * length;
+        int x24 = x2;
+        int y24 = y2 + length;
+        int x31 = x3 + length;
+        int y31 = y3;
+        int x32 = x3 + 2 * length;
+        int y32 = y3 + length;
+        int x33 = x3 + length;
+        int y33 = y3 + 2 * length;
+        int x34 = x3;
+        int y34 = y3 + length;
+        int x41 = x4 + length;
+        int y41 = y4;
+        int x42 = x4 + 2 * length;
+        int y42 = y4 + length;
+        int x43 = x4 + length;
+        int y43 = y4 + 2 * length;
+        int x44 = x4;
+        int y44 = y4 + length;
+        length = length / 3;
+        drawRectangles(graphics, x11, y11, length);
+        drawRectangles(graphics, x12, y12, length);
+        drawRectangles(graphics, x13, y13, length);
+        drawRectangles(graphics, x14, y14, length);
+        drawRectangles(graphics, x21, y21, length);
+        drawRectangles(graphics, x22, y22, length);
+        drawRectangles(graphics, x23, y23, length);
+        drawRectangles(graphics, x24, y24, length);
+        drawRectangles(graphics, x31, y31, length);
+        drawRectangles(graphics, x32, y32, length);
+        drawRectangles(graphics, x33, y33, length);
+        drawRectangles(graphics, x34, y34, length);
+        drawRectangles(graphics, x41, y41, length);
+        drawRectangles(graphics, x42, y42, length);
+        drawRectangles(graphics, x43, y43, length);
+        drawRectangles(graphics, x44, y44, length);
+    }
+
+
+    public static void drawRectangles(Graphics g, int x0, int y0, int length) {
+        g.drawRect(x0 + length, y0, length, length);
+        g.drawRect(x0 + 2 * length, y0+length, length, length);
+        g.drawRect(x0 + length, y0 + 2 * length, length, length);
+        g.drawRect(x0, y0 + length, length, length);
+    }
+
+
+    // Don't touch the code below
+    static int WIDTH = 1620;
+    static int HEIGHT = 1620;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
