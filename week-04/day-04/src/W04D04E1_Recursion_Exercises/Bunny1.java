43eeaf26d30e9c78ff5866341436e52b0e055f73
Mark Palotas <ppmark@gmail.com>
2018-11-15 11:02:22 +0100

    Added recursion exercise 06 Bunny2

--- week-04/day-04/src/W04D04E1_Recursion_Exercises/Bunny1.java
+++ week-04/day-04/src/W04D04E1_Recursion_Exercises/Bunny1.java
@@ -1,6 +1,6 @@
 package W04D04E1_Recursion_Exercises;
 
-// We have a number of bunnies and each bunny has two big floppy ears.
+// We have a number of bunnies and each bunny2 has two big floppy ears.
 // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
 
 public class Bunny1 {
--- week-04/day-04/src/W04D04E1_Recursion_Exercises/Power.java
+++ week-04/day-04/src/W04D04E1_Recursion_Exercises/Power.java
@@ -1,7 +1,7 @@
 package W04D04E1_Recursion_Exercises;
 
 // Given base and n that are both 1 or more, compute recursively (no loops)
-// the value of base to the n bunny, so powerN(3, 2) is 9 (3 squared).
+// the value of base to the n bunny2, so powerN(3, 2) is 9 (3 squared).
 
 public class Power {
     public static void main(String[] args) {
