61c4dbfdf9b8fa59446a359a6afe0cc4969a0da2
Mark Palotas <ppmark@gmail.com>
2018-11-15 10:54:12 +0100

    Added recursion exercise 05

---
+++ week-04/day-04/src/W04D04E1_Recursion_Exercises/Bunny1.java
@@ -0,0 +1,18 @@
+package W04D04E1_Recursion_Exercises;
+
+// We have a number of bunnies and each bunny has two big floppy ears.
+// We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+
+public class Bunny1 {
+    public static void main(String[] args) {
+        System.out.println(bunny(15));
+    }
+
+    public static int bunny(int n) {
+        if (n == 1) {
+            return 2;
+        } else {
+            return 2 + bunny(n - 1);
+        }
+    }
+}
