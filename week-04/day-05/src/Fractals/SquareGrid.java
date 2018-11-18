834e1807eef4e08c8c07654f4888a56548a55519
Mark Palotas <ppmark@gmail.com>
2018-11-18 18:21:22 +0100

    Finished SquareGrid Fractal exercise

--- week-04/day-05/src/Fractals/SquareGrid.java
+++ week-04/day-05/src/Fractals/SquareGrid.java
@@ -8,21 +8,30 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class SquareGrid {
     public static void mainDraw(Graphics graphics) {
         squareBorder(graphics, WIDTH / 4, WIDTH / 4, WIDTH / 2, 60);
+        drawSqGridFractal(graphics, WIDTH / 4, WIDTH / 4, WIDTH / 4, 30, 7);
     }
 
-    public static void drawSqGridFractal(Graphics g, int x0, int y0, int sideLength, int counter) {
+    public static void drawSqGridFractal(Graphics g, int x0, int y0, int sideLength, int border, int counter) {
         if (counter == 0) {
             return;
         } else {
-
+            squareBorder(g, x0 - sideLength / 2, y0 - sideLength / 2, sideLength, border);
+            drawSqGridFractal(g, x0 - sideLength / 2, y0 - sideLength / 2, sideLength / 2, border / 2, counter - 1);
+            squareBorder(g, x0 + 3 * sideLength / 2, y0 - sideLength / 2, sideLength, border);
+            drawSqGridFractal(g, x0 + 3 * sideLength / 2, y0 - sideLength / 2, sideLength / 2, border / 2, counter - 1);
+            squareBorder(g, x0 + 3 * sideLength / 2, y0 + 3 * sideLength / 2, sideLength, border);
+            drawSqGridFractal(g, x0 + 3 * sideLength / 2, y0 + 3 * sideLength / 2, sideLength / 2, border / 2, counter - 1);
+            squareBorder(g, x0 - sideLength / 2, y0 + 3 * sideLength / 2, sideLength, border);
+            drawSqGridFractal(g, x0 - sideLength / 2, y0 + 3 * sideLength / 2, sideLength / 2, border / 2, counter - 1);
         }
     }
 
     public static void squareBorder(Graphics g, int x0, int y0, int sidelength, int border) {
         g.setColor(Color.BLACK);
-        g.fillRect(x0,y0,sidelength,sidelength);
-        g.setColor(Color.WHITE);
-        g.fillRect(x0 + border, y0 + border, sidelength - 2 * border, sidelength - 2 * border);
+        g.fillRect(x0, y0, sidelength, border);
+        g.fillRect(x0 + sidelength - border, y0, border, sidelength);
+        g.fillRect(x0, y0, border, sidelength);
+        g.fillRect(x0, y0 + sidelength - border, sidelength, border);
     }
 
     // Don't touch the code below
