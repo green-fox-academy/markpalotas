4cef247e606a93e01912a8a74ea7869a9acad067
Mark Palotas <ppmark@gmail.com>
2018-12-05 14:03:46 +0100

    initialize springStart Application

---
+++ week-06_Practice_Previous Weeks Exercises/src/RandomAlphaNumeric/RandAlphaNum.java
@@ -0,0 +1,19 @@
+package RandomAlphaNumeric;
+
+public class RandAlphaNum {
+    public static void main(String[] args) {
+        System.out.println(randomAlphaNumeric(8));
+
+    }
+
+    private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    public static String randomAlphaNumeric(int count) {
+        StringBuilder builder = new StringBuilder();
+        while (count-- != 0) {
+            int character = (int)(Math.random()*ALPHA_NUMERIC_STRING.length());
+            builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+        }
+        return builder.toString();
+    }
+}
