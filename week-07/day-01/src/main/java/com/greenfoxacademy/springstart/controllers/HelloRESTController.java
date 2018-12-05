c718e3263bc59223696b0f9096987fa3abaaa1c4
Mark Palotas <ppmark@gmail.com>
2018-12-05 15:52:04 +0100

    add Count API Calls functionality

--- week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
+    AtomicLong atomicId = new AtomicLong();
 
     @RequestMapping(value = "/greeting")
     public Greeting greeting(@RequestParam("name") String name) {
-        return new Greeting(0, name);
+        return new Greeting(atomicId.getAndAdd(1), name);
     }
 }
