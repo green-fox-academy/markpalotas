45276cfd84d73b83f8866cee179c948fc9f552ff
Mark Palotas <ppmark@gmail.com>
2018-12-13 17:47:10 +0100

    further SQL exercises for week-08

--- week-08/day-02/StanfordRating.sql
+++ week-08/day-02/StanfordRating.sql
@@ -134,6 +134,26 @@ GROUP BY Movie.mID) AS aft1980
 ON bef1980.mID=aft1980.mID);
 
 
+SELECT (SELECT AVG(result.avgStar)
+FROM
+(SELECT Movie.mID, AVG(stars) AS avgStar
+FROM Movie
+JOIN Rating
+ON Movie.mID=Rating.mID
+WHERE Movie.year < 1980
+GROUP BY Movie.mID) AS result)
+-
+(SELECT AVG(result.avgStar)
+FROM
+(SELECT Movie.mID, AVG(stars) AS avgStar
+FROM Movie
+JOIN Rating
+ON Movie.mID=Rating.mID
+WHERE Movie.year > 1980
+GROUP BY Movie.mID) AS result);
+
+
+
 
 Rating Extras
 
@@ -202,6 +222,211 @@ ORDER BY namesAndTitles ASC;
 
 Q4
 
-Find the titles of all movies not reviewed by Chris Jackson. 
+'Find the titles of all movies not reviewed by Chris Jackson.'
 
-SELECT
+SELECT DISTINCT Movie.title
+FROM Movie
+JOIN Rating
+ON Movie.mID=Rating.mID
+JOIN Reviewer
+ON Reviewer.rID=Rating.rID
+WHERE Reviewer.name NOT LIKE "Chris Jackson";
+
+SELECT *
+FROM Movie
+JOIN Rating
+ON Movie.mID=Rating.mID
+JOIN Reviewer
+ON Reviewer.rID=Rating.rID
+
+SELECT DISTINCT Movie.title
+FROM Movie
+JOIN Rating
+ON Movie.mID=Rating.mID
+WHERE Rating.rID != 205;
+
+SELECT 
+(SELECT DISTINCT Movie.title
+FROM movie
+JOIN Rating
+ON Movie.mID=Rating.mID
+WHERE Rating.rID != 205)
+-
+(SELECT DISTINCT Movie.title
+FROM Movie
+JOIN Rating
+ON Movie.mID=Rating.mID
+WHERE Rating.rID=205);
+
+'ERROR 1242 (21000): Subquery returns more than 1 row'
+
+SELECT Movie.title
+FROM movie
+LEFT JOIN Rating
+ON Movie.mID=Rating.mID
+EXCEPT
+SELECT Movie.title
+FROM Movie
+JOIN Rating
+ON Movie.mID=Rating.mID
+WHERE Rating.rID=205;
+
+'above query works in SQLite, but not in mySQL (no EXCEPT keyword in mySQL)'
+
+SELECT DISTINCT Movie.title, Movie.mID
+FROM Movie
+LEFT JOIN Rating
+ON Movie.mID=Rating.mID
+WHERE Rating.mID
+NOT IN 
+(SELECT Rating.mID
+FROM Rating
+WHERE Rating.rID=205);
+
+'fails to give back the movies that werent reviewed at all'
+'because WHERE condition is on Rating.mID and not Movie.mID'
+
+SELECT DISTINCT Movie.title
+FROM Movie
+LEFT JOIN Rating
+ON Movie.mID=Rating.mID
+WHERE Movie.mID
+NOT IN 
+(SELECT Rating.mID
+FROM Rating
+WHERE Rating.rID=205);
+
+'this works but its supposed to be inefficient'
+
+SELECT DISTINCT Movie.title
+FROM Movie
+LEFT JOIN Rating
+ON Movie.mID=Rating.mID
+WHERE rID IS NULL
+OR 
+rID!=205;
+
+SELECT DISTINCT Movie.title, Movie.mID
+FROM Movie
+LEFT JOIN Rating
+ON Movie.mID=Rating.mID
+WHERE rID IS NULL;
+
+SELECT Movie.title, rID
+FROM Movie
+JOIN Rating
+ON Movie.mID=Rating.mID
+ORDER BY Movie.title ASC;
+
+SELECT *
+FROM Movie
+LEFT JOIN Rating
+ON Movie.mID=Rating.mID
+ORDER BY Movie.title ASC;
+
+SELECT Movie.title, rID
+FROM Movie
+LEFT JOIN Rating
+ON Movie.mID=Rating.mID
+GROUP BY Movie.mID
+HAVING rID=205
+ORDER BY Movie.title ASC;
+
+SELECT Movie.title,
+GROUP_CONCAT(rID)
+FROM Movie
+LEFT JOIN Rating
+ON Movie.mID=Rating.mID
+GROUP BY Movie.mID;
+
+SELECT title
+FROM 
+(SELECT Movie.title,
+GROUP_CONCAT(rID) AS concat
+FROM Movie
+LEFT JOIN Rating
+ON Movie.mID=Rating.mID
+GROUP BY Movie.mID) as unFiltered
+WHERE concat NOT LIKE "%205%"
+OR concat IS NULL;
+
+'this last one is working and does not use NOT IN,
+but *does* use NOT LIKE and GROUP_CONCAT, sooo...'
+
+
+
+Q5
+
+'For all pairs of reviewers such that both reviewers 
+gave a rating to the same movie, return the names of
+both reviewers. Eliminate duplicates, dont pair reviewers
+with themselves, and include each pair only once. For each
+pair, return the names in the pair in alphabetical order'
+
+
+
+
+Q6
+
+'For each rating that is the lowest (fewest stars) 
+currently in the database, return the reviewer name,
+movie title, and number of stars.'
+
+
+SELECT Reviewer.name, title, stars
+FROM Rating
+LEFT JOIN Reviewer
+ON Rating.rID=Reviewer.rID
+JOIN Movie
+On Rating.mID=Movie.mID;
+
+SELECT name, title, stars
+FROM Rating
+LEFT JOIN Reviewer
+ON Rating.rID=Reviewer.rID
+JOIN Movie
+On Rating.mID=Movie.mID
+GROUP BY Movie.mID
+HAVING stars=MIN(stars);
+
+'completely misunderstood the task, data should be returned
+for the movies having the lowest star rating in the database altogether,
+not per movie'
+
+SELECT name, title, MIN(stars) as stars
+FROM Rating
+JOIN Reviewer
+ON Rating.rID=Reviewer.rID
+JOIN Movie
+ON Rating.mID=Movie.mID
+GROUP BY Movie.mID
+HAVING stars= (SELECT MIN(stars) FROM Rating);
+
+'the above DOES work in mySQL, but fails in SQLite, returns completely different results'
+
+SELECT name, title, stars
+FROM Rating
+JOIN Reviewer
+ON Rating.rID=Reviewer.rID
+JOIN Movie
+ON Rating.mID=Movie.mID
+ORDER BY stars;
+
+SELECT name, title, stars
+FROM Rating
+JOIN Reviewer
+ON Rating.rID=Reviewer.rID
+JOIN Movie
+ON Rating.mID=Movie.mID
+ORDER BY stars
+LIMIT COUNT(SELECT mID FROM Rating WHERE );
+
+SELECT name, title, stars
+FROM Rating
+JOIN Reviewer
+ON Rating.rID=Reviewer.rID
+JOIN Movie
+ON Rating.mID=Movie.mID
+WHERE stars = (SELECT MIN(stars) FROM Rating);
+
+'this last one works as expected both in SQLite and mySQL'
